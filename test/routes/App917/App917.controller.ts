489d216e2cb5dfcf0fd61bfbe838f2c5b35c9e18
chelizichen <1347290221@qq.com>
2022-10-06 14:21:01 +0800

    秒杀案例

--- test/routes/App917/App917.controller.ts
+++ test/routes/App917/App917.controller.ts
@@ -2,14 +2,15 @@ import { HandleController } from "../../../lib/handle.class";
 import { Controller } from "../../../lib/handle.controller";
 import { Curd } from "../../../lib/handle.curd";
 import { Inject } from "../../../lib/handle.inject";
-import { Get } from "../../../lib/handle.method";
+import { Get, Post } from "../../../lib/handle.method";
 import { Pipe } from "../../../lib/handle.pipe";
 import { Ret, user } from "./App917.enity";
 import { useIdPipe, userNamePipe } from "./App917.pipe";
 import { App917Service } from "./App917.service";
 import * as mysql from "mysql";
 import { UseCache } from "../../../lib/handle.cache";
-import { createClient } from "redis";
+import { createClient, RedisClientType } from "redis";
+import { Body } from "../../../lib/types";
 
 const config = {
   host: "localhost",
@@ -32,7 +33,7 @@ class App917Controller extends HandleController {
   App917Service!: App917Service;
 
   @UseCache(createClient())
-  Redis!: any;
+  Redis!: RedisClientType;
 
   @Get("/a1")
   public async a1() {
@@ -64,6 +65,62 @@ class App917Controller extends HandleController {
 
   @Curd("/user", user, coon)
   public async CurdUser() {}
+
+  @Post("/seckill")
+  public async SecKill(
+    req: Body<{ uId: string; proId: string }>,
+    _res: Response
+  ) {
+    const body = req.body;
+    // console.log(this.Redis);
+    // 1 获取库存
+    const getRest = await this.App917Service.getRestKey(this.Redis, body.proId);
+    // 2 获取用户
+    const getUserKey = await this.App917Service.getUserKey(
+      this.Redis,
+      body.uId
+    );
+    if (getRest.total === null) {
+      return {
+        msg: "还没开始",
+      };
+    }
+    // 4 判断用户是否重复操作
+    const hasMember = await this.Redis.sIsMember(getUserKey, body.uId);
+    if (hasMember) {
+      return {
+        msg: "您已秒杀成功，不能重复操作",
+      };
+    }
+    // 5 如果库存数量少于1 则停止操作
+    if (getRest.total <= 0) {
+      return {
+        msg: "已无库存",
+      };
+    }
+
+    if (getRest.total >= 1) {
+      await this.Redis.decr(getRest.key);
+      await this.Redis.sAdd(getUserKey, body.uId);
+      return {
+        msg: "秒杀成功",
+      };
+    }
+    // 6 确认操作 并且付款到账以后 并入数据库里面生成数据
+
+    return Ret.Message(0, "success", "data");
+  }
+  @Post("/addProd")
+  public async addProd(
+    req: Body<{ proId: string; total: string }>,
+    _res: Response
+  ) {
+    const key = `sk:${req.body.proId}:qt`;
+    await this.Redis.set(key, req.body.total);
+    return {
+      msg: "设置成功",
+    };
+  }
 }
 
 export { App917Controller };
--- test/routes/App917/App917.service.ts
+++ test/routes/App917/App917.service.ts
@@ -1,3 +1,4 @@
+import { RedisClientType } from "@redis/client";
 import { Collect, Inject } from "../../../lib/handle.inject";
 import { App917Mapper } from "./App917.mapper";
 
@@ -10,6 +11,32 @@ class App917Service {
     const opt = ["19"];
     const ret = await this.App917Mapper.userList(opt);
     return ret;
+  }
+  public async getRestKey(
+    redis: RedisClientType<any, any, any>,
+    ProdId: string
+  ) {
+    const key = `sk:${ProdId}:qt`;
+    let data = await redis.get(key);
+    if (!data) {
+      data = null;
+    }
+    return {
+      total: Number(data),
+      key,
+    };
+  }
+  public getUserKey(_redis: RedisClientType<any, any, any>, UID: string) {
+    const key = `sk:${UID}:qt`;
+    return key;
+  }
+  public async SecKill() {
+    // 1 拼接库存key
+    // 2 拼接秒杀成功key
+    // 3 获取库存key 如果 Key == null 则库存还没有开始
+    // 4 判断用户是否重复操作
+    // 5 如果库存数量少于1 则停止操作
+    // 6 确认操作 并且付款到账以后 并入数据库里面生成数据
   }
 }
 

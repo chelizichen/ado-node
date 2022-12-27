daccadc5788414187fc1dda3fe63c12fbc101218
chelizichen <1347290221@qq.com>
2022-12-27 21:28:31 +0800

    删除文件相关注释

--- lib/ioc/controller.ts
+++ lib/ioc/controller.ts
@@ -1,24 +1,4 @@
 
-// type QueryId = {
-//   id: string;
-// }
-// @Controller("/computer")
-// class ComputerController extends AdoNodeController{
-//   @Get("/list")
-//   async getList(){
-//     return await {}
-//   }
-
-//   @Get("/one")
-//   async getOne(@Query() query:QueryId) {
-//     return await {}
-//   }
-
-//   @Post("/update")
-//   async update(@Body() body:Computer){
-//     return await {}
-//   }
-// }
 import { ref } from "./ref";
 
 import { GenereateRouter, SerivceMap } from "./service";

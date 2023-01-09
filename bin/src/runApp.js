6f2b133c1f9fdb25f94907c0b0ed329da33ab76e
chelizichen <1347290221@qq.com>
2023-01-09 18:55:17 +0800

    v3.1.5 更新

--- bin/src/runApp.js
+++ bin/src/runApp.js
@@ -5,21 +5,9 @@ const path = require('path');
 const os = require("os")
 function runViteApp() {
   const currProcess = spawn("vite", {
-    stdio: "pipe",
+    stdio: "inherit",
     shell: true,
     env: process.env,
-  });
-  currProcess.stdout.on("data", function (chunk) {
-    console.log(chalk.green("vite 客户端", new Date(), chunk.toString()));
-  });
-  currProcess.stderr.on("data", function (chunk) {
-    console.log(
-      chalk.red(
-        "错误！！！： vite 客户端运行输出",
-        new Date(),
-        chunk.toString()
-      )
-    );
   });
 }
 
@@ -27,70 +15,26 @@ function runNodeApp() {
   const currProcess = spawn(
     "ts-node-dev --project tsconfig.server.json src/index.ts",
     {
-      stdio: "pipe",
+      stdio: "inherit",
       shell: true,
       env: process.env,
     }
   );
-
-  currProcess.stdout.on("data", function (chunk) {
-    console.log(chalk.blue("node 服务端", new Date(), chunk.toString()));
-  });
-
-  currProcess.stderr.on("data", function (chunk) {
-    console.log(
-      chalk.red(
-        "错误！！！： node 服务端运行输出",
-        new Date(),
-        chunk.toString()
-      )
-    );
-  });
 }
 
 function buildViteApp() {
   const currProcess = spawn("vite build", {
-    stdio: "pipe",
+    stdio: "inherit",
     shell: true,
     env: process.env,
-  });
-  currProcess.stdout.on("data", function (chunk) {
-    console.log(
-      chalk.green("vite 客户端打包输出", new Date(), chunk.toString())
-    );
-  });
-  currProcess.stderr.on("data", function (chunk) {
-    console.log(
-      chalk.red(
-        "错误！！！： vite 客户端打包输出",
-        new Date(),
-        chunk.toString()
-      )
-    );
   });
 }
 
 function buildNodeApp() {
   const currProcess = spawn("tsc --project tsconfig.server.json", {
-    stdio: "pipe",
+    stdio: "inherit",
     shell: true,
     env: process.env,
-  });
-
-  currProcess.stdout.on("data", function (chunk) {
-    console.log(
-      chalk.green("node 服务端打包输出", new Date(), chunk.toString())
-    );
-  });
-
-  currProcess.stderr.on("data", function (chunk) {
-    console.log(
-      chalk.red(
-        "错误！！！： node 服务端打包输出",
-        new Date(),
-        chunk.toString()
-      )
-    );
   });
 }
 

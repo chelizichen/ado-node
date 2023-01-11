eb79a84fbf2bef009b3d19f3cc7ccfa2712aa7a4
chelizichen <1347290221@qq.com>
2023-01-11 12:34:36 +0800

    定时删除

--- lib/rpc/cache.ts
+++ lib/rpc/cache.ts
@@ -20,13 +20,13 @@ class ArcCache {
   public cache: Buffer; // 中位
   private middle: number; // 当前位
   private curr_size: number; // 当前元素个数
-  private unchange_size: number; // 不动位
+  private unchange_size: number; // 不动位 +1 是因为加一个head
   static proto = cache_buffer;
   constructor(public readonly size: number, public readonly timeout: number) {
     this.regular();
     this.middle = size / 2;
     this.curr_size = 1;
-    this.unchange_size = size / 10;
+    this.unchange_size = size / 10 + 1; // 
     if (this.unchange_size == 0) {
       this.unchange_size = 1;
     }
@@ -55,9 +55,9 @@ class ArcCache {
         let find_end = this.cache.lastIndexOf(">")
         let _cache =
           ArcCache.proto[_index + 1] + "<" + key + ">" + "<" + value + ">";
-        this.splice(find_end+1, 1, _cache)
+        this.splice(find_end + 1, 1, _cache)
         this.curr_size++
-        this.del(key,_index+1)
+        this.del(key, _index + 1)
         break;
       }
       let split_buffer = this.cache.subarray(curr_item_index, next_item_index);
@@ -65,7 +65,7 @@ class ArcCache {
         let _cache =
           ArcCache.proto[_index] + "<" + key + ">" + "<" + value + ">";
         this.curr_size++
-        this.del(key,_index)
+        this.del(key, _index)
         this.splice(curr_item_index + proto_len, 0, _cache);
         break;
       }
@@ -87,15 +87,15 @@ class ArcCache {
       if (next_item_index == -1) {
         next_item_index = this.cache.length
         let last_index = this.cache.lastIndexOf(">")
-        split_buffer = this.cache.subarray(curr_item_index, last_index+1);
-      }else{
+        split_buffer = this.cache.subarray(curr_item_index, last_index + 1);
+      } else {
         split_buffer = this.cache.subarray(curr_item_index, next_item_index);
       }
       let { key: _key, value: _value } = this.unpacking(split_buffer);
       if (_key == key) {
         value = _value;
         index = _index;
-        this.del(key,index,true)
+        this.del(key, index, true)
         break;
       }
     }
@@ -109,38 +109,45 @@ class ArcCache {
    * @description 删除键
    * @description 删除策略 保存当前下标 和键，一定时间之后再取出来，如果该键相等，则删除，否则认为该键为活跃键
    */
-  del(key:string,index:number,isGet:boolean):void;
-  del(key:string,index:number):void;
-  del(key:string,index:number,isGet?:boolean):void{
-    setTimeout(()=>{
+  del(key: string, index: number, isGet: boolean): void;
+  del(key: string, index: number): void;
+  del(key: string, index: number, isGet?: boolean): void {
+    setTimeout(() => {
+      // 不善前面的
+
+      if (this.curr_size < this.middle || this.unchange_size > index) {
+        return;
+      }
       let item = ArcCache.proto[index]
       let len = ArcCache.proto[index].length
-      let next = ArcCache.proto[index+1]
+      let next = ArcCache.proto[index + 1]
 
       let curr_index = this.cache.indexOf(item)
       let next_index = this.cache.indexOf(next)
 
-      if(next_index == -1){
-        next_index = this.cache.lastIndexOf(">")+1
+      if (next_index == -1) {
+        next_index = this.cache.lastIndexOf(">") + 1
       }
-      
-      let sub_cache = this.cache.subarray(curr_index+len,next_index)
-      let { key:_key} = this.unpacking(sub_cache)
-      
+
+      let sub_cache = this.cache.subarray(curr_index + len, next_index)
+      let { key: _key } = this.unpacking(sub_cache)
+
       // 如果key 值相等 则删除
-      if(_key == key){
-        this.splice(curr_index+len,next_index);
+      if (_key == key) {
+        console.log("删除完之后", this.cache.toString());
+        this.splice(curr_index + len, next_index - curr_index - len);
         this.curr_size--;
+        console.log(this.curr_size);
       }
-    },isGet?this.timeout*2:this.timeout)
+    }, isGet ? this.timeout * 2 : this.timeout)
   }
   /**
    * @description 定时删除
    */
-  regular() { 
-    setInterval(()=>{
+  regular() {
+    setInterval(() => {
       console.log(this.cache.toString());
-    },3000)
+    }, 3000)
   }
 
   private unpacking(buffer: Buffer): {
@@ -195,27 +202,27 @@ class ArcCache {
         symmetry_buf_index + len,
         next_symmetry_index
       );
-      
+
       // 交换到前面
       this.splice(
         symmetry_buf_index + len,
         sp_buffer.length,
         sp_buffer
       );
-      
+
       // 交换到后面
       let after_index_len = ArcCache.proto[index].length;
       let after_index = this.cache.indexOf(ArcCache.proto[index]);
       let after_next_index = this.cache.indexOf(ArcCache.proto[index + 1]);
-      
+
       // 判断是否为尾节点
-      if(after_next_index == -1){
-        after_next_index = this.cache.lastIndexOf(">")+1
+      if (after_next_index == -1) {
+        after_next_index = this.cache.lastIndexOf(">") + 1
       }
 
       this.splice(
         after_index + after_index_len,
-        after_next_index-after_index-after_index_len,
+        after_next_index - after_index - after_index_len,
         symmetry_buffer
       );
     }
@@ -223,22 +230,22 @@ class ArcCache {
   /**
    * @description 类似于 Array.prototype.splice 因为 Buffer 中没有 所以提供了这个方法
    */
-  private splice(index: number, subIndex: number, buf: Buffer|string):void 
-  private splice(index: number, subIndex: number) :void
-  private splice(index: number, subIndex: number, buf?: Buffer|string) {
+  private splice(index: number, subIndex: number, buf: Buffer | string): void
+  private splice(index: number, subIndex: number): void
+  private splice(index: number, subIndex: number, buf?: Buffer | string) {
     if (typeof buf == "string") {
       buf = Buffer.from(buf)
     }
 
     let buffer_start: Buffer | null = this.cache.subarray(0, index);
-  
+
     let buffer_end: Buffer | null = this.cache.subarray(
       subIndex + index,
-      this.cache.lastIndexOf(">")+1
+      this.cache.lastIndexOf(">") + 1
     );
-    if(buf){
+    if (buf) {
       this.cache = Buffer.concat([buffer_start, buf, buffer_end]);
-    }else{
+    } else {
       this.cache = Buffer.concat([buffer_start, buffer_end]);
     }
     buffer_start = null;
@@ -261,9 +268,9 @@ a.set("j", "j");
 a.set("k", "k");
 a.set("l", "l");
 a.set("m", "m");
-a.get("k")
-a.get("m")
-
+a.get("b")
+a.get("c");
+a.get("d");
 
 export {
   ArcCache
